3a807cc38af81a468d873f1f4077af3407cf55e4
Kumar Saksham <everydaycodings@gmail.com>
2021-07-14 12:56:53 +0530

    Hello World

---
+++ main.py
@@ -0,0 +1,28 @@
+import discord
+import requests
+import json
+from backend import *
+import config
+
+token = config.discord_api
+api_key = config.weather_api
+client = discord.Client()
+command_prefix = "."
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='.[location]'))
+
+@client.event
+async def on_message(message):
+    if message.author != client.user and message.content.startswith(command_prefix):
+        if len(message.content.replace(command_prefix, '')) >= 1:
+            location = message.content.replace(command_prefix, '').lower()
+            url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&units=metric&appid={api_key}&units=imperial'
+            try:
+                data = parse_data(json.loads(requests.get(url).content)['main'])
+                await message.channel.send(embed=weather_message(data, location))
+            except KeyError:
+                await message.channel.send(embed=error_message(location))
+
+client.run(token)
